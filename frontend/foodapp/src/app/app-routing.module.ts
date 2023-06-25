2afd93b543ef53d894c9a8755b61bb0112c10080
Daniel Matos <deme12.1@gmail.com>
2023-06-24 20:35:32 -0500

    modificada la estructura para que concuerde con Angular

--- frontend/foodapp/src/app/app-routing.module.ts
+++ frontend/foodapp/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ListaproductosComponent } from './components/listaproductos/listaproductos.component';
 
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'home', component: HomeComponent},
-  {path:'login', component: LoginComponent}
+  {path:'login', component: LoginComponent},
+  {path:'productos', component: ListaproductosComponent}
 ];
 
 @NgModule({
